c508aa2e4d71afbbffc774861761d0bdd7be8dd4
Ali Najafizadeh <ali@blockthrough.com>
2023-12-19 13:41:29 -0500

    Add base interface for self update toolchain

---
+++ selfupdate.go
@@ -0,0 +1,60 @@
+package selfupdate
+
+import (
+	"context"
+	"io"
+)
+
+type Signer interface {
+	Sign(ctx context.Context, r io.Reader) io.Reader
+}
+
+type Uploader interface {
+	Upload(ctx context.Context, version string, r io.Reader) error
+}
+
+type Checker interface {
+	Check(ctx context.Context, currentVersion string) (newVersion string, desc string, err error)
+}
+
+type Downloader interface {
+	Download(ctx context.Context, version string) io.Reader
+}
+
+type Verifier interface {
+	Verify(ctx context.Context, r io.Reader) io.Reader
+}
+
+type Patcher interface {
+	Patch(ctx context.Context, patch io.Reader) error
+}
+
+type Runner interface {
+	Run(ctx context.Context) error
+}
+
+// helper types
+
+type SignerFunc func(ctx context.Context, r io.Reader) io.Reader
+
+var _ Signer = SignerFunc(nil)
+
+func (f SignerFunc) Sign(ctx context.Context, r io.Reader) io.Reader {
+	return f(ctx, r)
+}
+
+type VerifierFunc func(ctx context.Context, r io.Reader) io.Reader
+
+var _ Verifier = VerifierFunc(nil)
+
+func (f VerifierFunc) Verify(ctx context.Context, r io.Reader) io.Reader {
+	return f(ctx, r)
+}
+
+type RunnerFunc func(ctx context.Context) error
+
+var _ Runner = RunnerFunc(nil)
+
+func (f RunnerFunc) Run(ctx context.Context) error {
+	return f(ctx)
+}
